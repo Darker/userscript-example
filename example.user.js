8a18cdf7643180ac66d6999a52aaf570adf6030e
Jakub Mareda <mareda@techsys.cz>
2017-08-28 22:06:15 +0200

    Include does not work again

--- example.user.js
+++ example.user.js
@@ -2,8 +2,8 @@
 // @name        Name
 // @namespace   i_never_figured_out_what_does_this_mean
 // @description Description
-// @include     https://example.net
-// @version     1
+// @include     https://example.net/*
+// @version     1.1
 // @grant       none
 // @downloadURL https://github.com/Darker/userscript-example/raw/master/example.user.js
 // @updateURL https://github.com/Darker/userscript-example/raw/master/example.user.js
