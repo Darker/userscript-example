a0d8c7974e9577a83cfdfb0b8c105d4039f278ed
Jakub Mareda <mareda@techsys.cz>
2017-08-28 21:57:28 +0200

    First commit

---
+++ example.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Name
+// @namespace   i_never_figured_out_what_does_this_mean
+// @description Description
+// @include     https://example.net
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+  /// code
