e1d14cc51d0539515e5b049191413c2421350057
Jakub Mareda <mareda@techsys.cz>
2017-08-28 22:30:54 +0200

    More info

--- example.user.js
+++ example.user.js
@@ -3,12 +3,18 @@
 // @namespace   i_never_figured_out_what_does_this_mean
 // @description Description
 // @include     https://example.net/*
-// @version     1.2
+// @version     1.3
 // @grant       none
 // @downloadURL https://github.com/Darker/userscript-example/raw/master/example.user.js
 // @updateURL https://github.com/Darker/userscript-example/raw/master/example.user.js
 // ==/UserScript==
 
+// Note that in most cases, updateURL and downloadURL
+// is NOT NECESSARY. Greasemonkey will automatically 
+// use the URL you used to download script
+// Wiki: https://wiki.greasespot.net/Metadata_Block#.40downloadURL
+
 // Use this to check if your script will update, among other things
-console.log(GM_info);
+// Wiki: https://wiki.greasespot.net/GM_info
+console.log("Script will "+(GM_info?"":"not ")+"update.");
   /// code
